4c77d6ed2a2238679a383c3ee817f7e540bfeffa
Diusburg <88924856+Diusburg@users.noreply.github.com>
2021-09-16 15:44:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+} 
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+} 
+
+// Animation
+
+anime({
+  targets: '.header .navbar a',
+  translateX: [100, 0],
+  duration: 1200,
+  opacity: [0, 1],
+  delay: (el, i) => {
+    return 300 + 100 * i;
+  },
+})
+
+// Scroll to top button
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.25) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
